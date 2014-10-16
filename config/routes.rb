2eae963d9b66a1ece2db7e2dfc339d4cf6dc54ee
steffb <steffenf.birkeland@gmail.com>
2014-10-15 18:42:49 -0700

    addaed bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'say/hello'
+
+  get 'say/goodbye'
+
   resources :communities
 
   # The priority is based upon order of creation: first created -> highest priority.
